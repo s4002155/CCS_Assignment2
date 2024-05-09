98121cf481ecb21069a0203e37c4d2f2ca2702f2
s4002155 <s4002155@student.rmit.edu.au>
2024-05-10 01:39:54 +1000

    description of changes made

--- public/script.js
+++ public/script.js
@@ -220,7 +220,7 @@
             s.p.add(s.v);
 
             // draw the ASCII art at the position
-            ctx2.fillStyle = 'darkgrey';
+            ctx2.fillStyle = 'navy';
             ctx2.font = '6px monospace'; // Adjust font size here
             // draw each line of ASCII art
             const lines = ascii_art.split('\n');
