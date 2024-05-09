bc2708c0bd171ed19661fcb48db9d469dc0aaeee
s4002155 <s4002155@student.rmit.edu.au>
2024-05-10 01:59:54 +1000

    description of changes made

--- public/script.js
+++ public/script.js
@@ -49,7 +49,7 @@
     
         display() {
             if (this.state != 2) {
-                renderer.stroke(c2.Color.rgb(230, .2));
+                renderer.stroke(c2.Color.rgb(50, .2));
                 renderer.lineWidth(2);
                 renderer.fill(this.color);
                 renderer.rect(this.p.x, this.p.y, this.randomWidth, this.randomHeight); // 랜덤한 너비와 높이로 직사각형 그리기
